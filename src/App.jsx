0938566a32533db2310f041c141fedc13e7f7c7b
Inshalmehboob <inshalkshf@gmail.com>
2025-09-22 16:41:35 +0500

    Fix case-sensitive import for WasifEngineeringWebsite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WasifEngineeringWebsite from "./components/WasifEngineeringWebsite";
+import WasifEngineeringWebsite from "./components/wasifengineeringwebsite";
 
 function App() {
   return (
